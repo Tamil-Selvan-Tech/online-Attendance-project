c5a0e6d2c241c29818fccafbaffa34cb72cbd275
Tamil Selvan <tamildharshini007@gmail.com>
2025-07-31 09:18:27 +0530

    Fixed base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/online-Attendance-project/', // 👈 Use your repo name here
+base: '/online-Attendance-project/', // 👈 Use your repo name here
 })
